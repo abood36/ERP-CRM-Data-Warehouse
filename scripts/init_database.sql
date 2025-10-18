191b391585bc776ef8c8b76cec057f410c1f5b50
Abdelrahman Mahrouss <137737000+abood36@users.noreply.github.com>
2025-10-18 19:25:01 +0300

    Create init_database.sql for DataWarehouse setup

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+-- CREATING DB & SCHEMAS --
+
+USE master;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
